f3a8d41c04adb902684057e2b778877fb1e1aa98
Vasanth M <108510927+Vasa-1410@users.noreply.github.com>
2022-08-24 13:29:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+def send_mail(thought):
+    password = "efcizpsocbrpmmfl"
+    with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+        connection.starttls()
+        connection.login(user="vasanthmuni14@gmail.com", password=password)
+        connection.sendmail(from_addr="vasanthmuni14@gmail.com",
+                            to_addrs="munivasanth10@gmail.com",
+                            msg=f"Subject:Thought of the Day\n\n{thought}")
+
+def send_bd(add,message):
+    password = "efcizpsocbrpmmfl"
+    with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+        connection.starttls()
+        connection.login(user="vasanthmuni14@gmail.com", password=password)
+        connection.sendmail(from_addr="vasanthmuni14@gmail.com",
+                            to_addrs=add,
+                            msg=f"Subject:Birthday Wishes\n\n{message}")
+#
+#
+now = dt.datetime.now()
+day = now.weekday()
+if day==0:
+    with open("C:/Users/Vasanth M/PycharmProjects/from_16/birthday_wishes/quotes.txt",mode="r") as quotes:
+        quote = quotes.readlines()
+        quote_list =[line.strip() for line in quote]
+        thought = random.choice(quote_list)
+        send_mail(thought)
+
+day = now.day
+month_now = now.month
+
+data = pandas.read_csv("C:/Users/Vasanth M/PycharmProjects/from_16/birthday_wishes/wishes.csv")
+for index,row in data.iterrows():
+    if row["date"]==day and row["month"] == month_now:
+        name_ = row["name"]
+        address = row["mail"]
+        with open("C:/Users/Vasanth M/PycharmProjects/from_16/birthday_wishes/letter_1.txt","r") as letter:
+            content = letter.read()
+            new_letter=content.replace("[name]",name_)
+        send_bd(address,new_letter)
+
